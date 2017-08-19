4971cd180f94a78792246ee548a8ed47bdc45f26
Icaro F Guselian <icaro.ifg@gmail.com>
2017-08-18 21:08:58 -0300

    Creating user entity

---
+++ WeddingAssist.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeddingAssist.Domain.Enums;
+
+namespace WeddingAssist.Domain.Entities
+{
+    class User
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string AwsUserId { get; private set; }
+        public ERegistrationStatus RegistrationStatus { get; private set; }
+        public bool Enable { get; private set; }
+    }
+}
+
---
+++ WeddingAssist.Domain/Enums/ERegistrationStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeddingAssist.Domain.Enums
+{
+    public enum ERegistrationStatus
+    {
+        CONFIRMED = 1,
+        UNCONFIRMED = 2
+    }
+}
