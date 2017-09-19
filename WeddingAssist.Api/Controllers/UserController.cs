d0c8a8b97143652483a367f46f268e9b0070b3e6
Renan Costa <renancitow@hotmail.com>
2017-09-19 01:58:38 -0300

    feat: created a web api

---
+++ WeddingAssist.Api/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WeddingAssist.Domain.Entities;
+using WeddingAssist.Domain.Infra;
+
+namespace WeddingAssist.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        [Route("check_user_info")]
+        public IActionResult CheckUserInfo([FromBody]User user)
+        {
+            UserRepository userRepository = new UserRepository();
+            userRepository.GetFianceByEmail("icaro.ifg@gmail.com");
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("teste")]
+        public IActionResult teste()
+        {
+            UserRepository userRepository = new UserRepository();
+            userRepository.GetFianceByEmail("icaro.ifg@gmail.com");
+            return Ok();
+        }
+    }
+}
