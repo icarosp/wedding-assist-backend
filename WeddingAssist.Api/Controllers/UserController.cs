9ba27a171d200c746ee251b5b9becf3f5d29d36f
Renan Costa <renancitow@hotmail.com>
2017-09-20 01:58:27 -0300

    feat: create endpoint to save user after registration

--- WeddingAssist.Api/Controllers/UserController.cs
+++ WeddingAssist.Api/Controllers/UserController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WeddingAssist.Domain.Entities;
 using WeddingAssist.Domain.Infra;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace WeddingAssist.Api.Controllers
 {
@@ -12,21 +14,35 @@ namespace WeddingAssist.Api.Controllers
     public class UserController : Controller
     {
         [HttpPost]
-        [Route("check_user_info")]
-        public IActionResult CheckUserInfo([FromBody]User user)
+        [Route("get_user_by_email")]
+        public IActionResult CheckUserInfo([FromBody]string email)
         {
             UserRepository userRepository = new UserRepository();
-            userRepository.GetFianceByEmail("icaro.ifg@gmail.com");
-            return Ok();
+            userRepository.GetUserByEmail(email);
+            return Ok(email);
+        }
+
+        [HttpPost]
+        [Route("save_fiance")]
+        public IActionResult SaveFiance([FromBody]Fiance fiance)
+        {
+            try
+            {
+                UserRepository userRepository = new UserRepository();
+                userRepository.SaveFiance(fiance);
+                return Created("SaveFiance", fiance);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e);
+            }
         }
 
         [HttpGet]
         [Route("teste")]
         public IActionResult teste()
         {
-            UserRepository userRepository = new UserRepository();
-            userRepository.GetFianceByEmail("icaro.ifg@gmail.com");
-            return Ok();
+            return Ok("|==============|\n|=API RODANDO!=|\n|==============|");
         }
     }
 }
