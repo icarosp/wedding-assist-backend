bbdfa20c5fab254bcb038a085e84fe49504669ba
Renan Costa <renancitow@hotmail.com>
2017-09-20 01:56:53 -0300

    feat: create method to save user in database

--- WeddingAssist.Domain/Infra/UserRepository.cs
+++ WeddingAssist.Domain/Infra/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 using WeddingAssist.Domain.Entities;
@@ -8,22 +9,62 @@ namespace WeddingAssist.Domain.Infra
 {
     public class UserRepository
     {
-        private readonly SqlConnection sqlConnection = new SqlConnection("server=wa-db-01.cwwhxvtrxmqx.us-east-1.rds.amazonaws.com,1433;user id=wassist;password=weddingassistfiap2017;database=db_wedding_assist");
+        private readonly string ConnectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
-        public Fiance GetFianceByEmail(string email)
+        public User GetUserByEmail(string email)
         {
-            Fiance fiance = new Fiance();
-            var command = new SqlCommand($"SELECT * FROM tb_user WHERE usr_email = '{email}'", sqlConnection);
-            sqlConnection.Open();
-            using (var reader = command.ExecuteReader())
+            User user = null;
+            using (var conn = new SqlConnection(ConnectionString))
             {
-                while (reader.Read())
+                using (var cmd = new SqlCommand($"SELECT * FROM tb_user usr LEFT JOIN tb_fiance fic ON fic.usr_id = usr.usr_id LEFT JOIN tb_provider prv ON prv.usr_id = usr.usr_id WHERE usr_email = '{email}'", conn))
                 {
-                    //build Fiance
+                    cmd.CommandType = CommandType.Text;
+                    
+                    conn.Open();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            //build Fiance
+                        }
+                    }
+                    conn.Close();
+
                 }
             }
-            sqlConnection.Close();
-            return fiance;
+            return user;
+        }
+
+        public void SaveFiance(Fiance fiance)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                using (var cmd = new SqlCommand("[dbo].[User-Insert-SaveFiance]", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    //User data
+                    cmd.Parameters.AddWithValue("@usr_email", fiance.Email);
+                    cmd.Parameters.AddWithValue("@usr_nickname", fiance.Nickname);
+                    cmd.Parameters.AddWithValue("@usr_phone", fiance.Phone);
+                    cmd.Parameters.AddWithValue("@usr_aws_user_id", fiance.AwsUserId);
+                    cmd.Parameters.AddWithValue("@rst_id", (int)fiance.RegistrationStatus);
+
+                    //Fiance data
+                    cmd.Parameters.AddWithValue("@fic_name", fiance.Name);
+                    cmd.Parameters.AddWithValue("@fic_birth", fiance.Birth);
+                    cmd.Parameters.AddWithValue("@gnd_id", (int)fiance.Gender);
+                    cmd.Parameters.AddWithValue("@fic_status", fiance.Enable);
+
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    if (rowsAffected < 1)
+                        throw new Exception("Error to save Fiance");
+
+                }
+            }
         }
     }
 }
