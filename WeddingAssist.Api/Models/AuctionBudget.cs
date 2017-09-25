439accf29a9067d5d717abec746b96ef15765bd6
Icaro F. Guselian <icaro.ifg@gmail.com>
2017-09-25 05:43:47 -0300

    fixing auctionbudget model mistake

--- WeddingAssist.Api/Models/AuctionBudget.cs
+++ WeddingAssist.Api/Models/AuctionBudget.cs
@@ -1,14 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WeddingAssist.Domain.Entities;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using WeddingAssist.Domain.Entities;
 
-namespace WeddingAssist.Api.Models
-{
-    public class AuctionBudget : Budget
-    {
-        public int NumberOfBids { get; set; }
-        public int IsActive { get; set; }
-    }
-}
+//namespace WeddingAssist.Api.Models
+//{
+//    public class AuctionBudget : Budget
+//    {
+//        public int NumberOfBids { get; set; }
+//        public int IsActive { get; set; }
+
+//        public string FormatedCreationDate { get
+//            {
+//                return this.StartDate.ToString("dd/MM/yyyy mm:hh");
+//            } set { }
+//        }
+
+//        public bool IsAuctionActive {
+//            get {
+//                if (IsActive < 1 || this.Duration > DateTime.Now)
+//                    return false;
+//                return true;
+//            } set { }
+//        }
+//    }
+//}
--- WeddingAssist.Domain/Entities/AuctionBudget.cs
+++ WeddingAssist.Domain/Entities/AuctionBudget.cs
@@ -8,8 +8,29 @@ namespace WeddingAssist.Domain.Entities
 {
     public class AuctionBudget : Budget
     {
+
+        public int AuctionId { get; set; }
         public int NumberOfBids { get; set; }
         public int IsActive { get; set; }
-        public int AuctionId { get; set; }
+
+        public string FormatedCreationDate
+        {
+            get
+            {
+                return this.StartDate.ToString("dd/MM/yyyy hh:mm");
+            }
+            set { }
+        }
+
+        public bool IsAuctionActive
+        {
+            get
+            {
+                if (IsActive < 1 || this.Duration > DateTime.Now)
+                    return false;
+                return true;
+            }
+            set { }
+        }
     }
 }
