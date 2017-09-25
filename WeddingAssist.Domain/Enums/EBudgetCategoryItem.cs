cba0c18fa230def53514ad210dacc183495f720d
Icaro F. Guselian <icaro.ifg@gmail.com>
2017-09-25 02:39:32 -0300

    Already receiving working budget model

--- WeddingAssist.Domain/Enums/EBudgetCategoryItem.cs
+++ WeddingAssist.Domain/Enums/EBudgetCategoryItem.cs
@@ -8,9 +8,14 @@ namespace WeddingAssist.Domain.Enums
     {
         BRAZILIAN = 1,
         ITALIAN = 2,
-        BEVERANGES = 3,
-        SMALL = 4,
-        MEDIUM = 5,
-        LARGE = 6,
+        ARABIAN = 3,
+        JAPANESE = 4,
+        KOREAN = 5,
+        CERIMONYCHAIR = 6,
+        EVENTCHAIR = 7,
+        BEER = 8,
+        JUICE = 9,
+        BEVERAGE = 10,
+        PARTYTABLE = 11
     }
 }
